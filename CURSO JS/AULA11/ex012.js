95751ca8a6c95a997208f4df130e39b73ff0c060
devgabijs <devgabijs@gmail.com>
2022-01-25 13:32:53 -0300

    Aula11

---
+++ CURSO JS/AULA11/ex012.js
@@ -0,0 +1,7 @@
+console.log('O console funcionou corretamente !')
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60) {// Condição Simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
