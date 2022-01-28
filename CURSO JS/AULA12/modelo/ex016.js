bfbbb4894358900f16a445e6929000ddcaf666e6
devgabijs <devgabijs@gmail.com>
2022-01-28 13:40:55 -0300

    exercício14

---
+++ CURSO JS/AULA12/modelo/ex016.js
@@ -0,0 +1,13 @@
+//var agora = new Date()
+var hora = 2
+console.log(`Agora são ${hora} horas.`)
+if ( hora > 6 && hora <= 11) {
+    console.log('Bom dia!')
+} else if ( hora > 012 && hora <= 17){
+        console.log ('Boa Tarde!')
+} else if (hora >= 18 && hora <= 23){
+        console.log ('Boa Noite!')
+}    if (hora >= 0  && hora <= 5){
+        console.log('Boa Madrugada!')
+    }
+   
