4a2683223bd566788bb839c97d6570d342d5e5f4
devgabijs <devgabijs@gmail.com>
2022-01-29 14:16:00 -0300

    Verificador de Idade

--- CURSO JS/AULA12/ex014/script.js
+++ CURSO JS/AULA12/ex014/script.js
@@ -1,1 +1,18 @@
-alert ("Olá!")
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+   //  var hora = data.getHours()
+   var hora = 20
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >=0 && hora < 12 ) {
+        //BOM DIA!
+        img.src = 'abacaximanha.jpg'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE!
+        img.src = 'tarde.jpg'
+    } else {
+        //BOA NOITE!
+        img.src = 'noite.jpg'
+    }
+}
---
+++ CURSO JS/AULA12/ex02.modelo/script.js
@@ -0,0 +1,11 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!') 
+    } else {
+        window.alert('tudo ok')
+    }
+}
